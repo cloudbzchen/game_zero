4c7bfbca3d3bcec8178e61fa4fef5f369b5f9685
shiyanlou_chen <bzchen_cn@foxmail.com>
2017-12-06 04:10:11 +0800

    beta

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@
 #include <unistd.h>
 
 using namespace std;
+
 void swap(int &a, int &b){
 	int t=a;
 	a = b;
 	b = t;
 }
-
+int getrand(int min, int max)
+{
+	return(min+rand()%(max-min+1));
+}
 
 WINDOW *create_newwin(int height,int width,int starty,int startx);
 void destory_win(WINDOW *local_win);
@@ -20,12 +24,13 @@ int game_win_height=35;
 int game_win_width=45;
 
 int hint_win_height,hint_win_width;
-WINDOW * game_win, *hint_win;
+WINDOW * game_win, *hint_win ,*score_win;
 int key;
 
 class Piece
 {
 public:
+	int score;
 	int shape; 
 	int next_shape;
 
@@ -35,31 +40,29 @@ public:
 	int size_h;
 	int size_w;
 
-	int box_shape[4][4];
-	int box_map[35][45];
-	int box_stay[35][35];
+	int next_size_h;
+	int next_size_w;
 
-	
+	int box_shape[4][4];
+	int next_box_shape[4][4];
+
+	int box_map[35][45];
+
+	bool game_over;
 
 public:
 	void initial();
-	void set_shape(int shape);
-	void set_map();
-	void showmap();
+	int set_shape(int &cshape, int box_shape[][4],int &size_w, int & size_h);
 
-
+	void score_next();
+	void judge();
 	void move();
 	void rotate();
-	bool judge_right();
-	bool judge_left();
-	bool stop();
 	bool isaggin();
-	void set_stop();
+	bool exsqr(int row);
+
 	
 };
-
-
-
 
 int main()
 {
@@ -82,7 +85,15 @@ int main()
 	wborder(game_win, '*', '*', '*', '*', '*', '*', '*', '*');
 	wrefresh(game_win);
 
-	hint_win = create_newwin(hint_win_height, hint_win_width, 5, game_win_width+10);
+	hint_win = create_newwin(hint_win_height, hint_win_width, 0, game_win_width+10);
+	mvprintw(0, game_win_width+10+2,"%s","Next");
+	refresh();
+
+	score_win = create_newwin(hint_win_height, hint_win_width, 20, game_win_width+10);
+	mvprintw(20, game_win_width+10+2,"%s","Score");
+	refresh();
+
+
 
 	Piece* pp = new Piece;
 	pp->initial();
@@ -90,14 +101,24 @@ int main()
 
 	while(1)
 	{
-		srand((unsigned)time(0));
-		pp->next_shape=rand()%7;
-		pp->set_map();
 		pp->move();
-		pp->showmap();
-
+		if(pp->game_over)
+			break;
 	}
 
+	destory_win(game_win);
+	destory_win(hint_win);
+	destory_win(score_win);
+	delete pp;
+	system("clear");
+
+	int row,col;
+	getmaxyx(stdscr,row,col);
+	mvprintw(row/2,col/2 ,"%s","GAMER OVER ! \n ");
+	mvprintw(row/2+2,col/2-2 ,"%s","Wait 5s to return tthe erminal ! \n ");
+	refresh();
+
+	sleep(5);
 	endwin();
 	return 0;
 }
@@ -121,11 +142,10 @@ void destory_win(WINDOW *local_win)
 
 void Piece::initial()
 {
-	this->head_x=game_win_width/2;
-	this->head_y=1;
+	score=0;
+	game_over=false;
 	for(int i =0;i<game_win_height;i++)
 		for(int j=0;j<game_win_width;j++){
-			box_stay[i][j]=0;
 			if(i==0 || i==game_win_height-1 || j==0 || j==game_win_width-1){
 				box_map[i][j]=1;
 			}
@@ -133,95 +153,121 @@ void Piece::initial()
 				box_map[i][j]=0;
 		}
 
-
-
-
 	srand((unsigned)time(0));
-	shape=rand()%7;
-	set_shape(shape);
+	shape=getrand(0,6);
+	set_shape(shape,box_shape,size_w,size_h);
 
+	next_shape=getrand(0,6);
+	set_shape(next_shape,next_box_shape,next_size_w,next_size_h);
+
+	for(int i =0;i<4;i++)
+		for(int j=0;j<4;j++)
+			if(next_box_shape[i][j]==1){
+				mvwaddch(hint_win,(hint_win_height-size_h)/2+i,(hint_win_width-size_w)/2+j,'#');
+				wrefresh(hint_win);
+			}
+				
+
+	mvwprintw(score_win, hint_win_height/2,hint_win_width/2-2,"%d",score);
+	wrefresh(score_win);
 }
 
-void Piece::set_shape(int shape)
+int Piece::set_shape(int &cshape, int shape[][4],int &size_w,int &size_h)
 {
 	int i,j;
 	for(i=0;i<4;i++)
 		for(j=0;j<4;j++)
-			box_shape[i][j]=0;
-	switch(shape)
+			shape[i][j]=0;
+	switch(cshape)
 	{
 		case 0:	
 			size_h=1;
 			size_w=4;	
-			this->box_shape[0][0]=1;
-			this->box_shape[0][1]=1;
-			this->box_shape[0][2]=1;
-			this->box_shape[0][3]=1;
+			shape[0][0]=1;
+			shape[0][1]=1;
+			shape[0][2]=1;
+			shape[0][3]=1;
 			break;
 		case 1:
 			size_h=2;
 			size_w=3;
-			this->box_shape[0][0]=1;
-			this->box_shape[1][0]=1;
-			this->box_shape[1][1]=1;
-			this->box_shape[1][2]=1;
+			shape[0][0]=1;
+			shape[1][0]=1;
+			shape[1][1]=1;
+			shape[1][2]=1;
 			break;
 		case 2:
 			size_h=2;
 			size_w=3;	
-			this->box_shape[0][2]=1;
-			this->box_shape[1][0]=1;
-			this->box_shape[1][1]=1;
-			this->box_shape[1][2]=1;
+			shape[0][2]=1;
+			shape[1][0]=1;
+			shape[1][1]=1;
+			shape[1][2]=1;
 			break;
 		case 3:
 			size_h=2;
 			size_w=3;
-			this->box_shape[0][1]=1;
-			this->box_shape[0][2]=1;
-			this->box_shape[1][0]=1;
-			this->box_shape[1][1]=1;
+			shape[0][1]=1;
+			shape[0][2]=1;
+			shape[1][0]=1;
+			shape[1][1]=1;
 			break;
 
 		case 4:
 			size_h=2;
 			size_w=3;
-			this->box_shape[0][0]=1;
-			this->box_shape[0][1]=1;
-			this->box_shape[1][1]=1;
-			this->box_shape[1][2]=1;
+			shape[0][0]=1;
+			shape[0][1]=1;
+			shape[1][1]=1;
+			shape[1][2]=1;
 			break;
 
 		case 5:	
 			size_h=2;
 			size_w=2;
-			this->box_shape[0][0]=1;
-			this->box_shape[0][1]=1;
-			this->box_shape[1][0]=1;
-			this->box_shape[1][1]=1;
+			shape[0][0]=1;
+			shape[0][1]=1;
+			shape[1][0]=1;
+			shape[1][1]=1;
 			break;
 
 		case 6:	
 			size_h=2;
 			size_w=3;
-			this->box_shape[0][1]=1;
-			this->box_shape[1][0]=1;
-			this->box_shape[1][1]=1;
-			this->box_shape[1][2]=1;
+			shape[0][1]=1;
+			shape[1][0]=1;
+			shape[1][1]=1;
+			shape[1][2]=1;
 			break;
 	}
 
+	head_x=game_win_width/2;
+	head_y=1;
+
+	if(isaggin())    /* GAME OVER ! */
+		game_over=true;
+
+
 }
+
+
 void Piece::rotate()
 {
-	int temp[4][4];
-	int i,j;
+	int temp[4][4]={0};
+	int temp_piece[4][4]={0};
+	int i,j,tmp_size_h,tmp_size_w;
 
-	if(!isaggin()){
-		for(i=0;i<4;i++)
-			for(j=0;j<4;j++)
-				temp[j][i]=box_shape[i][j];
+	tmp_size_w=size_w;
+	tmp_size_h=size_h;
 
+	for(int i=0; i<4;i++)
+		for(int j=0;j<4;j++)
+			temp_piece[i][j]=box_shape[i][j];
+
+
+	for(i=0;i<4;i++)
+		for(j=0;j<4;j++)
+			temp[j][i]=box_shape[i][j];
 	i=size_h;
 	size_h=size_w;
 	size_w=i;
@@ -229,8 +275,31 @@ void Piece::rotate()
 		for(j=0;j<size_w;j++)
 			box_shape[i][size_w-1-j]=temp[i][j];
 
-	}
 
+	if(isaggin()){
+		for(int i=0; i<4;i++)
+			for(int j=0;j<4;j++)
+				box_shape[i][j]=temp_piece[i][j];
+		size_w=tmp_size_w;
+		size_h=tmp_size_h;
+	}
+	else{
+		for(int i=0; i<4;i++)
+			for(int j=0;j<4;j++){
+				if(temp_piece[i][j]==1){
+					mvwaddch(game_win,head_y+i,head_x+j,' ');
+					wrefresh(game_win);
+				}
+			}
+		for(int i=0; i<size_h;i++)
+			for(int j=0;j<size_w;j++){
+				if(this->box_shape[i][j]==1){
+					mvwaddch(game_win,head_y+i,head_x+j,'#');
+					wrefresh(game_win);
+				}
+		}
+
+	}
 }
 
 void Piece::move(){
@@ -243,93 +312,93 @@ void Piece::move(){
 	 timeout.tv_usec= 500000;
 	      
   	if (select(1, &set, NULL, NULL, &timeout) == 0){
-		// for(int i=3; i>=0;i--)
-		// 	for(int j=0;j<4;j++){
-		// 		if(this->box_shape[i][j]==1){
-		// 			mvwaddch(game_win,head_y+i,head_x+j,' ');
-		// 			mvwaddch(game_win,head_y+i+1,head_x+j,'#');
+		head_y++;
+		if(isaggin()){
+			head_y--;
+			for(int i=0;i<size_h;i++)
+				for(int j=0;j<size_w;j++)
+					if(box_shape[i][j]==1)
+						box_map[head_y+i][head_x+j]=1;
+			score_next();
+		}
+		else{
+			for(int i=size_h-1; i>=0;i--)
+				for(int j=0;j<size_w;j++){
+					if(this->box_shape[i][j]==1){
+						mvwaddch(game_win,head_y-1+i,head_x+j,' ');
+						mvwaddch(game_win,head_y+i,head_x+j,'#');
 
-		// 		}
-		// 	}
-		// wrefresh(game_win);
-		this->head_y++;
+					}
+				}
+			wrefresh(game_win);
+		}
+
   	}
 	      
 	if (FD_ISSET(0, &set)) {
 	        while ((key = getch()) == -1) ;
 
 		if(key==KEY_LEFT){
-			// if(judge_left() && !stop()){
-			// 	for(int i=0; i<4;i++)
-			// 		for(int j=0;j<4;j++){
-			// 			if(this->box_shape[i][j]==1){
-			// 				mvwaddch(game_win,head_y+i,head_x+j,' ');
-			// 				mvwaddch(game_win,head_y+i,head_x+j-1,'#');
-
-			// 			}
-			// 		}
-
-			// 	wrefresh(game_win);
-			// 	this->head_x--;
 			head_x--;
 			if(isaggin())
 				head_x++; //undo
+			else{
+				for(int i=0; i<size_h;i++)
+					for(int j=0;j<size_w;j++){
+						if(this->box_shape[i][j]==1){
+							mvwaddch(game_win,head_y+i,head_x+j+1,' ');
+							mvwaddch(game_win,head_y+i,head_x+j,'#');
+
+						}
+					}
+				wrefresh(game_win);
+			}
 		}
 
 		if(key==KEY_RIGHT){
-			// if(judge_right() && !stop()){
-			// 	for(int i=0; i<4;i++)
-			// 		for(int j=3;j>=0;j--){
-			// 			if(this->box_shape[i][j]==1){
-			// 				mvwaddch(game_win,head_y+i,head_x+j,' ');
-			// 				mvwaddch(game_win,head_y+i,head_x+j+1,'#');
-
-			// 			}
-			// 		}
-
-			// 	wrefresh(game_win);
-			// 	this->head_x++;
 			head_x++;
 			if(isaggin())
 				head_x--;
+			else{
+				for(int i=0; i<size_h;i++)
+					for(int j=size_w-1;j>=0;j--){
+						if(this->box_shape[i][j]==1){
+							mvwaddch(game_win,head_y+i,head_x+j-1,' ');
+							mvwaddch(game_win,head_y+i,head_x+j,'#');
+
+						}
+					}
+				wrefresh(game_win);
+			}
 		}
 
 		if(key==KEY_DOWN){
-			// if(!stop()){
-			// 	usleep(100);
-			// 	for(int i=3; i>=0;i--)
-			// 		for(int j=0;j<4;j++){
-			// 			if(this->box_shape[i][j]==1){
-			// 				mvwaddch(game_win,head_y+i,head_x+j,' ');
-			// 				mvwaddch(game_win,head_y+i+1,head_x+j,'#');
-
-			// 			}
-			// 		}
-			// 	wrefresh(game_win);
-			// 	this->head_y++;
-			// }
 			head_y++;
 			if(isaggin()){
 				head_y--;
 				for(int i=0;i<size_h;i++)
 					for(int j=0;j<size_w;j++)
 						if(box_shape[i][j]==1)
-							box_stay[head_y+i][head_x+j]=1;
-				head_x=game_win_width/2;
-				head_y=1;
+							box_map[head_y+i][head_x+j]=1;
+
+				score_next();
+				
+			}
+			else{
+				for(int i=size_h-1; i>=0;i--)
+					for(int j=0;j<size_w;j++){
+						if(this->box_shape[i][j]==1){
+							mvwaddch(game_win,head_y-1+i,head_x+j,' ');
+							mvwaddch(game_win,head_y+i,head_x+j,'#');
+
+						}
+					}
+				wrefresh(game_win);
 			}
 		}
 
 		if(key==KEY_UP)
-		{
-			// for(int i=0;i<4;i++)
-			// 	for(int j=0;j<4-i;j++)
-			// 		swap(this->box_shape[i][j],this->box_shape[4-1-j][4-1-i]);
-			// for(int i=0;i<4/2;i++)
-			// 	for(int j=0;j<4;j++)
-			// 		swap(this->box_shape[i][j],this->box_shape[4-1-i][j]);
 			rotate();
-		}
 
 		if(head_x+size_w+1>game_win_width)
 			head_x=game_win_width-size_w-1;
@@ -338,115 +407,94 @@ void Piece::move(){
 	}
 }
 
-	/* detect the wall */
-
-// bool Piece::judge_right(){
-// 	//the right wall
-// 	for(int j=3;j>=0;j--)
-// 		for(int i=0;i<4;i++)
-// 			if(this->box_shape[i][j]==1)
-// 				if((head_x+j+2)==game_win_width || box_map[head_y+i][head_x+j+1]==1)
-// 					return false;
-// 				else
-// 					return true;
-// }
-
-// bool Piece::judge_left(){
-// 	//the left wall
-// 	for(int j=0;j<4;j++)
-// 		for(int i=0;i<4;i++)
-// 			if(this->box_shape[i][j]==1)
-// 				if((head_x+j-1)==0 || box_map[head_y+i][head_x+j-1]==1)
-// 					return false;
-// 				else
-// 					return true;
-// }
-
-// bool Piece::stop(){
-
-// 	//detect the bottom box
-// 	for(int i=3; i>=0;i--)
-// 		for(int j=0;j<4;j++){
-// 			if(this->box_shape[i][j]==1)
-// 				if((head_y+i+2)==game_win_height){
-// 					set_stop();
-// 					return true;
-// 				}
-// 		}
-// 	for(int i=3; i>=0;i--)
-// 		for(int j=0;j<4;j++){
-// 			if(this->box_shape[i][j]==1)
-// 				if(box_map[head_y+i+1][head_x+j]==1){
-// 					set_stop();
-// 					return true;
-// 				}
-// 		}
-
-// 	return false;
-// }
-
-// void Piece::set_stop(){
-
-// 	for(int i=0; i<4;i++)
-// 		for(int j=0;j<4;j++){
-// 			if(this->box_shape[i][j]==1)
-// 				this->box_map[head_y+i][head_x+j]=1;
-// 		}
-// 	head_x=game_win_width/2;
-// 	head_y=1;
-// 	shape=next_shape;
-// 	set_shape(shape);
-
-// }
-
 bool Piece::isaggin(){
 	for(int i=0;i<size_h;i++)
 		for(int j=0;j<size_w;j++){
 			if(box_shape[i][j]==1){
-				if(head_y+i+2>game_win_height)
+				if(head_y+i > game_win_height-2)
 					return true;
-				if(box_stay[head_y+i][head_x+j]==1)
+				if(head_x+j > game_win_width-2 || head_x+i-1<0)
+					return true;
+				if(box_map[head_y+i][head_x+j]==1)
 					return true ;
 			}
-
 		}
-
 	return false;
 }
 
-void Piece::set_map(){
-	for(int i =0;i<game_win_height;i++)
-		for(int j=0;j<game_win_width;j++){
-			if(i==0 || i==game_win_height-1 || j==0 || j==game_win_width-1)
-				box_map[i][j]=1;
-			else
+bool Piece::exsqr(int row){
+	for(int j=1;j<game_win_width-1;j++)
+		if(box_map[row][j]==1)
+			return true;
+	return false;
+}
+
+void Piece::judge(){
+	int i,j;
+	int line=0;
+	bool full;
+	for(i=1;i<game_win_height-1;i++){
+		full=true;
+		for(j=1;j<game_win_width-1;j++){
+			if(box_map[i][j]==0)
+				full=false;
+		}
+		if(full){
+			line++;
+			score+=50;
+			for(j=1;j<game_win_width-1;j++)
 				box_map[i][j]=0;
 		}
-
-	for(int i=0;i<size_h;i++)
-		for(int j=0;j<size_w;j++)
-			box_map[head_y+i][head_x+j]=box_shape[i][j];
-
-	for(int i =0;i<game_win_height;i++)
-		for(int j=0;j<game_win_width;j++){
-			if(box_stay[i][j]==1) 	
-				box_map[i][j]=1;
-
-		}
-
-}
-void Piece::showmap(){
-	for(int i =1;i<game_win_height-1;i++)
-		for(int j=1;j<game_win_width-1;j++){
-			if(box_map[i][j]==1){
-				mvwaddch(game_win,i,j,'#');
-				wrefresh(game_win);
-			}
-			else{
-				mvwaddch(game_win,i,j,' ');
-				wrefresh(game_win);
-
+	}
+	if(line!=0){
+	for(i=game_win_height-2;i>=2;i--){
+		int s=i;
+		if(exsqr(i)==0){
+			while(s>1 && exsqr(--s)==0);
+			for(j=1;j<game_win_width-1;j++){
+				box_map[i][j]=box_map[s][j];
+				box_map[s][j]=0;
 			}
 		}
+	}
 
+	for(int i=1;i<game_win_height-1;i++)
+			for(int j=1;j<game_win_width-1;j++){
+				if(box_map[i][j]==1){
+					mvwaddch(game_win,i,j,'#');
+					wrefresh(game_win);
+				}
+				else{
+					mvwaddch(game_win,i,j,' ');
+					wrefresh(game_win);
+				}
+			}
+	}
+}			
+
+void Piece::score_next(){
+	score+=10;
+	judge();
+
+	mvwprintw(score_win, hint_win_height/2,hint_win_width/2-2,"%d",score);
+	wrefresh(score_win);
+
+
+	set_shape(next_shape,box_shape,size_w,size_h);
+
+	this->next_shape=getrand(0,6);
+	set_shape(next_shape,next_box_shape,next_size_w,next_size_h);
+
+
+	for(int i =1;i<hint_win_height-1;i++)
+		for(int j=1;j<hint_win_width-1;j++){
+			mvwaddch(hint_win, i, j,' ');
+			wrefresh(hint_win);
+		}
+	for(int i =0;i<4;i++)
+		for(int j=0;j<4;j++)
+			if(next_box_shape[i][j]==1){
+				mvwaddch(hint_win,(hint_win_height-size_h)/2+i,(hint_win_width-size_w)/2+j,'#');
+				wrefresh(hint_win);
+		}
 }
